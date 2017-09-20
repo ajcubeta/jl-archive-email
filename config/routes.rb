5b4f0aba090ada6d01fea6951c793e44d99485cd
Andre Joseph <ajcubeta@gmail.com>
2017-09-20 18:22:21 +0800

    Test using devise auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users, :path_names => { :sign_in => 'login', :sign_out => 'logout' }
+  # https://github.com/plataformatec/devise/blob/master/README.md
+  devise_scope :user do
+    get '/login',          to: 'devise/sessions#new'
+    get '/login',          to: 'devise/sessions#destroy'
+    get '/password/reset', to: 'devise/passwords#new'
+  end
+
+  get 'home/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "home#index"
 end
