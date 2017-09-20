0c45db9cb41d5a82e9ce213c4aea61f08ca8d645
Andre Joseph <ajcubeta@gmail.com>
2017-09-20 18:36:26 +0800

    Landing page and normalization css

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  layout :choose_layout
+
+  private
+
+  def choose_layout
+    devise_controller? ? 'landing' : 'application'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,13 @@ Rails.application.routes.draw do
     get '/password/reset', to: 'devise/passwords#new'
   end
 
+  # devise_for :users, :path_names => { :sign_in => 'login', :sign_out => 'logout' }
+  # devise_scope :user do
+  #   get '/login'          => 'devise/sessions#new',   :as => 'new_user_session'
+  #   get '/logout'         => 'sessions#destroy',      :as => 'destroy_user_session'
+  #   get '/password/reset' => 'devise/passwords#new',  :as => 'new_user_password'
+  # end
+
   get 'home/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
